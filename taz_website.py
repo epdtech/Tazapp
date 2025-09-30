2b85bdd04bfe12a53082e7aafe9b142ecda7dd3f
epdtech <118341264+epdtech@users.noreply.github.com>
2025-09-30 11:13:21 +0200

    Update taz_website.py

--- taz_website.py
+++ taz_website.py
@@ -7,7 +7,7 @@ api_key = st.secrets["GOOGLE_API_KEY"]
 genai.configure(api_key=api_key)
 
 
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-2.0-flash')
 
 
 
@@ -107,3 +107,4 @@ with col2:
 st.write("CONTACT")
 st.title("For any inquiries, please contac me at:")
 st.write("taz@taz.com")
+
